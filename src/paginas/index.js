fa583478c3f7ebe60073e7f2b5792dc1d52c6b7b
billybrian <billybrianm@hotmail.com>
2021-09-16 10:57:09 -0300

    redirecionar usuario para login caso nao esteja logado

---
+++ src/paginas/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './home';
+import Usuario from './usuario';
+import Login from '../components/login/login';
+
+const Paginas = () => {
+    const [token, setToken] = useState();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+      }
+
+
+    return(
+        <Router>
+            <Route exact path="/" component= {Login} />
+            <Route path = "/usuario" component = {Usuario} />
+        </Router>
+    );
+};
+export default Paginas;
