437630bae30b711df12a818cb8da80c76a19fb40
billybrian <billybrianm@hotmail.com>
2021-09-16 10:55:22 -0300

    mudança do app.js para dar controle ao componente de paginas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-
-class App extends React.Component {
-  componentDidMount() {
-    const apiUrl = '/api/usuarios';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('Dados: ', data._embedded.usuarios[0]));
-  }
-  render() {
-    return <h1>Componente test no console </h1>;
-  }
+import Paginas from './paginas';
+function App() {
+  return (
+    <div>
+      <Paginas />
+    </div>
+  );
 }
 export default App;
