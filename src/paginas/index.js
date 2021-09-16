f88e85d95296d6ee40bb2e486d0d77a1eca97aca
billybrian <billybrianm@hotmail.com>
2021-09-16 16:54:13 -0300

    permite tela de cadastro sobrescrever tela de login quando não logado

--- src/paginas/index.js
+++ src/paginas/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
 } from "react-router-dom";
 import Home from './home';
 import Usuario from './usuario';
 import Login from '../components/login/login';
 import AtualizarToken from '../AtualizarToken';
+import Cadastro from './cadastro';
+import { createBrowserHistory } from "history";
 
 
 const Paginas = () => {
+    const history = createBrowserHistory()
     const { token, setToken } = AtualizarToken();
 
     if(!token) {
-        return <Login setToken={setToken} />
-      }
+        if(history.location.pathname === '/cadastro') {
+            return <Cadastro />
+        } else {
+            return <Login setToken={setToken} />
+        }
+    }
 
 
     return(
         <Router>
             <Route exact path="/" component= {Home} />
             <Route path = "/usuario" component = {Usuario} />
+            <Route path = "/cadastro" component = {Cadastro} />
         </Router>
     );
 };
