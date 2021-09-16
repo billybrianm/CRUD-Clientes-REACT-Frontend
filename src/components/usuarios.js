d9fad5537655da011f6878cdccb648d3bba618df
billybrian <billybrianm@hotmail.com>
2021-09-16 12:22:08 -0300

    adição de key no array de usuários na listagem

--- src/components/usuarios.js
+++ src/components/usuarios.js
@@ -6,18 +6,19 @@
       return (
         <div>
           <center><h1>Lista de Usuários</h1></center>
-          {usuarios.map((usuario) => (
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">{usuario.nome}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{usuario.usuario}</h6>
-                <p class="card-text">CPF: {usuario.cpf}</p>
-                <p class="card-text">CEP: {usuario.cep}</p>
-                <p class="card-text">Endereço: {usuario.logradouro}</p>
-                <p class="card-text">Complemento: {usuario.cpf}</p>
-                <p class="card-text">Bairro: {usuario.bairro}</p>
-                <p class="card-text">Cidade: {usuario.cidade}</p>
-                <p class="card-text">UF: {usuario.uf}</p>
+          {usuarios.map((usuario, index) => (
+              
+            <div key={index} className="card">
+              <div className="card-body">
+                <h5 className="card-title">{usuario.nome}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{usuario.usuario}</h6>
+                <p className="card-text">CPF: {usuario.cpf}</p>
+                <p className="card-text">CEP: {usuario.cep}</p>
+                <p className="card-text">Endereço: {usuario.logradouro}</p>
+                <p className="card-text">Complemento: {usuario.cpf}</p>
+                <p className="card-text">Bairro: {usuario.bairro}</p>
+                <p className="card-text">Cidade: {usuario.cidade}</p>
+                <p className="card-text">UF: {usuario.uf}</p>
               </div>
             </div>
           ))}
