4e9a34fb2787b8065cebdf191f05f47d08452155
billybrian <billybrianm@hotmail.com>
2021-09-17 14:02:21 -0300

    varias alteraçoes no cadastro
    - validação completa de todos os campos
    - cep buscando na api
    - email multiplo e apenas validos
    - fix no required do cpf
    - formatacao do cep
    - fix do select do tipo telefones

--- src/paginas/cadastro.js
+++ src/paginas/cadastro.js
@@ -16,7 +16,7 @@ const Cadastro = () => {
     const [telefoneList, setTelefoneList] = useState([{ telefone: "", tipoTelefone: "" }]);
     const [emailList, setEmailList] = useState([{ email: ""}]);
     
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { tipoSelecionado, setValue, register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
     const [carregando, setCarregando] = React.useState(true);
@@ -24,6 +24,10 @@ const Cadastro = () => {
         { label: "Carregando ...", value: "" }
       ]);
     const [valorTiposTelefone, setValorTiposTelefone] = React.useState("CELULAR");
+
+    const [cep, setCep] = useState();
+
+    const [telefoneMask, setTelefoneMask] = React.useState("(99)99999-9999");
 
     React.useEffect(() => {
         let unmounted = false;
@@ -63,7 +67,7 @@ const Cadastro = () => {
     }
 
     const emailValido = (email) => {
-        if (validator.isEmail(email)) { // valido
+        if (validator.isEmail(email.email)) { // valido
             return true;
         } else { // invalido
             return false;
@@ -72,60 +76,81 @@ const Cadastro = () => {
 
     const handleChange = e => setVal(e.target.value);
 
-    const handleChangeCEP = e => setVal(e.target.value);
-
     const handleEmailRemove = index => {
         const list = [...emailList];
         list.splice(index, 1);
         setEmailList(list);
     };
 
-    const handleEmailAdd = (e, index) => {
+    const handleEmailAdd = (x, index) => {
 
-        if(validateEmail(e, index)) {
+        if(emailValido(x)) {
+            console.log(...emailList);
             setEmailList([...emailList, { email: ""}]);
+            setValue('emails'+emailList.length, '');
         } else {
             console.log("E-mail inválido");
         }
     };
 
-    const handleTelefoneChange = (e, index) => {
-        const { name, value } = e.target;
-        const list = [...telefoneList];
-        list[index][name] = value;
-        setTelefoneList(list);
-      };
+    const formatar = (value, pattern) => {
+        let i = 0,
+            v = value.toString();
+        let result =  pattern.replace(/#/g, _ => v[i++]);
+        if (value.replace(/\s/g, '').length < 8) {
+          return result.substr(0, result.indexOf('u')); 
+        }
+          return result; 
+      }
 
-    const handleTelefoneRemove = index => {
-        const list = [...telefoneList];
-        list.splice(index, 1);
-        setTelefoneList(list);
-    };
-    
-    const handleTelefoneAdd = (e, index) => {
-        setTelefoneList([...telefoneList, { telefone: "", tipoTelefone: "" }]);
-    };
-
-
-    const handleInputChange = (e, index) => {
-        const { name, value } = e.target;
-        const list = [...inputList];
-        list[index][name] = value;
-        setInputList(list);
-      };
+    const mascaraCEP = ({target}) => {
+        if(target.value.length >= 9) {
+            buscarCEP(target.value);
+        }
+        if(cep && (target.value.length < cep.length)) {
+            setCep(target.value)
+            return;
+          }
+          setCep(formatar(target.value.replace(/\s/g, '').replace(/\D/g, ''), '##### ###'))
+    }
 
 
-    // handle click event of the Remove button
-    const handleRemoveClick = index => {
-        const list = [...inputList];
-        list.splice(index, 1);
-        setInputList(list);
-    };
-    
-    // handle click event of the Add button
-    const handleAddClick = () => {
-        setInputList([...inputList, { firstName: "", lastName: "" }]);
-    };
+    async function buscarCEP(cepe) {
+        let cepFormatado = cepe.replace(/\s/g, "").substr(0, 8);
+        return fetch('https://viacep.com.br/ws/'+cepFormatado+'/json/',
+        {
+            method: "GET",
+            mode: 'cors',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            },
+        }).then((response) => response.json()).then((responseData) => {
+            setValue('logradouro', responseData.logradouro);
+            setValue('bairro', responseData.bairro);
+            setValue('cidade', responseData.localidade);
+            setValue('complemento', responseData.complemento);
+            setValue('uf', responseData.uf);
+
+        }).catch(error => console.warn(error));
+    }
+
+    const selecionadoNovoTipoTelefone = (e) => {
+
+        let tipo = e.target.options[e.target.options.selectedIndex].value;
+
+        if(tipo == "COMERCIAL") {
+            setTelefoneMask("(99)9999-9999");
+        } else if (tipo == "RESIDENCIAL") {
+            setTelefoneMask("(99)9999-9999");
+        } else {
+            setTelefoneMask("(99)99999-9999");
+        }
+        
+
+        setValorTiposTelefone(e.target.options[e.target.options.selectedIndex].value);
+    }
+      
 
 
 return(
@@ -148,41 +173,59 @@ return(
 
                   <label className="" autoComplete="off" >Senha <span>*</span></label>
                   <input type="password" name="" placeholder="Digite sua senha..." {...register("senha", { required: true })}/> 
-                  {errors.nome?.type === 'required' && <span>O campo senha é obrigatório.<br/></span>}
+                  {errors.senha?.type === 'required' && <span>O campo senha é obrigatório.<br/></span>}
 
                   <label className="" >CPF <span>*</span></label>
-                  <InputMask mask="999.999.999-99" onChange={handleChange} type="text" placeholder="Digite seu CPF..." {...register("cpf", { required: true })}/>
-                  {errors.nome?.type === 'required' && <span>O campo CPF é obrigatório.<br/></span>}
-
+                  <InputMask mask="999.999.999-99" onChange={handleChange} type="text" placeholder="Digite seu CPF..." {...register("cpf", { required: true, pattern: /\d{3}\.\d{3}\.\d{3}-\d{2}/ })}/>
+                  {errors.cpf?.type === 'required' && <span>O campo CPF é obrigatório.<br/></span>}
+                  {errors.cpf?.type === 'pattern' && <span>Preencha o CPF por completo.<br/></span>}
                   <label className="" >CEP <span>*</span></label>
-                  <InputMask mask="99999-999" onChange={handleChangeCEP} type="text" placeholder="Digite seu CEP..." {...register("cep", { required: true })}/>
+
+                  <input 
+                    required
+                    placeholder="00000-000"
+                    type="text"
+                    inputMode="numeric"
+                    autoComplete="cep"
+                    name="cep"
+                    id="cep"
+                    onChange={mascaraCEP}
+                    value={cep}
+                    />
+
+                
                   {errors.nome?.type === 'required' && <span>O campo CEP é obrigatório.<br/></span>}
+                  {errors.nome?.type === 'minLength' && <span>Preencha o CEP por completo.<br/></span>}
 
                   <label className="" >Endereço <span>*</span></label>
                   <input type="text" name="" placeholder="Digite seu logradouro..." {...register("logradouro", { required: true })}/> 
-                  {errors.nome?.type === 'required' && <span>O campo endereço é obrigatório.<br/></span>}
+                  {errors.logradouro?.type === 'required' && <span>O campo endereço é obrigatório.<br/></span>}
 
                   <label className="" >Bairro <span>*</span></label>
                   <input type="text" name="" placeholder="Digite seu bairro..." {...register("bairro", { required: true })}/> 
-                  {errors.nome?.type === 'required' && <span>O campo bairro é obrigatório.<br/></span>}
+                  {errors.bairro?.type === 'required' && <span>O campo bairro é obrigatório.<br/></span>}
 
                   <label className="" >Cidade <span>*</span></label>
                   <input type="text" name="" placeholder="Digite sua cidade..." {...register("cidade", { required: true })}/> 
-                  {errors.nome?.type === 'required' && <span>O campo cidade é obrigatório.<br/></span>}
+                  {errors.cidade?.type === 'required' && <span>O campo cidade é obrigatório.<br/></span>}
 
                   <label className="" >UF <span>*</span></label>
                   <input type="text" name="" placeholder="Digite sua UF..." {...register("uf", { required: true })}/> 
-                  {errors.nome?.type === 'required' && <span>O campo UF é obrigatório.<br/></span>}
+                  {errors.uf?.type === 'required' && <span>O campo UF é obrigatório.<br/></span>}
 
-                  <label className="" {...register("telefones", { required: true })}>Telefone(s) <span>*</span></label>
+                  <label className="" >Complemento</label>
+                  <input type="text" name="" placeholder="Digite o complemento" {...register("complemento")}/> 
+
+                  <label className="">Telefone(s) <span>*</span></label>
 
 
                   <div className="side-by-side">
-                    <input type="text" placeholder="Digite seu telefone..." /> 
+                    <InputMask mask={telefoneMask} type="text" {...register("telefones", { required: true })} placeholder="Digite seu telefone..." /> 
                     <select style={{width: 200}}
                         disabled={carregando}
                         value={valorTiposTelefone}
-                        onChange={e => setValorTiposTelefone(e.currentTarget.value)}>
+                        onChange={(e) => selecionadoNovoTipoTelefone(e)}
+                        ref={tipoSelecionado}>
 
                         {tiposTelefone.map(({ label, value }) => (
                             <option key={value} value={value}>
@@ -192,23 +235,23 @@ return(
                     </select>
                     <button>+</button> 
                   </div>
-                  {errors.nome?.type === 'required' && <span>Pelo menos um telefone deve ser preenchido.<br/></span>}
+                  {errors.telefones?.type === 'required' && <span>Pelo menos um telefone deve ser preenchido.<br/></span>}
 
-                  <label className="" {...register("emails", { required: true })}>E-mail(s) <span>*</span></label>                                    
+                  <label className="">E-mail(s) <span>*</span></label>                                    
 
                   {emailList.map((x, i) => {
                             return (
                             <div key={i} className="side-by-side">
-                                <input type="text" placeholder="Digite seu e-mail..." value={x.email} onChange={(e) => validateEmail(e, i)}/> 
+                                <input type="text" {...register("emails"+i)} placeholder="Digite seu e-mail..." value={x.email} onChange={(e) => validateEmail(e, i)}/> 
                                 {emailList.length !== 1 && <button className="mr10" type="button" id={i} onClick={() => handleEmailRemove(i)}>-</button>}
-                                {emailList.length - 1 === i && <button type="button" onClick={(e) => handleEmailAdd(e, x)}>+</button>}
+                                {emailList.length - 1 === i && <button type="button" onClick={(e) => handleEmailAdd(x, i)}>+</button>}
                             </div>
                             );
                         })}                
-                  {errors.nome?.type === 'required' && <span>Pelo menos um e-mail deve ser preenchido.<br/></span>}
+                  {errors.emails?.type === 'required' && <span>Pelo menos um e-mail deve ser preenchido.<br/></span>}
                   <div className="side-by-side">
                     <button name="" value="Voltar" type="button" onClick={() => { window.location.replace("/"); }}> Voltar </button>
-                    <input type="submit" name="" value="Registrar" href="#" />
+                    <input type="submit" name="" value="Registrar" />
                   </div>
               </form>
             </div>
